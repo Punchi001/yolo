90bf56abcbde104f032f07a9d7d443bdc136966a
Kenneth Njehu <k.njehu@gmail.com>
2023-04-02 15:23:22 -0700

    configured my server.

--- backend/upload.js
+++ backend/upload.js
@@ -7,16 +7,16 @@ const path = require('path');
 // Set Storage engine
 const storage = multer.diskStorage({
     destination: './public/images/',
-    filename: function(req, file, cb){
-        cb(null,file.originalname);
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
     }
 })
 
 // Init upload
 const upload = multer({
     storage: storage,
-    limits: {fileSize: 1000000},
-    fileFilter: function(req,file, cb){
+    limits: { fileSize: 1000000 },
+    fileFilter: function(req, file, cb) {
         checkFileType(file, cb);
     }
 }).single('image');
@@ -24,17 +24,17 @@ const upload = multer({
 
 
 // Check file type
-function checkFileType(file, cb){
-    // Alowed ext
+function checkFileType(file, cb) {
+    // Allowed ext
     const fileType = /jpeg|jpg|png|gif/;
     // Check ext
-    const extname = fileType.test( path.extname(file.originalname).toLowerCase());
+    const extname = fileType.test(path.extname(file.originalname).toLowerCase());
     // check mime
     const mimetype = fileType.test(file.mimetype);
 
-    if(mimetype && extname){
+    if (mimetype && extname) {
         return cb(null, true);
-    }else{
+    } else {
         cb('Error: Images Only!!')
     }
 }
