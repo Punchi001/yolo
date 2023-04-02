35122082e1b07ad048675b32a8dfc634f581be51
Kenneth Njehu <k.njehu@gmail.com>
2023-04-02 13:57:42 -0700

    modifed server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,44 +8,35 @@ const upload = multer();
 
 const productRoute = require('./routes/api/productRoute');
 
-
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'yolomy';
+let mongodb_url = process.env.MONGODB_URI;
 
 // define a url to connect to the database
-const MONGODB_URI = process.env.MONGODB_URI || mongodb_url + dbName
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-let db = mongoose.connection;
-
-// Check Connection
-db.once('open', () => {
-    console.log('Database connected successfully')
-})
-
-// Check for DB Errors
-db.on('error', (error) => {
-    console.log(error);
-})
+mongoose.connect(mongodb_url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Database connected successfully');
+  })
+  .catch((err) => {
+    console.log(`Error connecting to the database: ${err}`);
+  });
 
 // Initializing express
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(express.json())
+app.use(express.json());
 
-// 
 app.use(upload.array());
 
 // Cors 
 app.use(cors());
 
 // Use Route
-app.use('/api/products', productRoute)
+app.use('/api/products', productRoute);
 
 // Define the PORT
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+});
